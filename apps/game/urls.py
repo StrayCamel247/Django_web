44a48a67326a853f26e06ccd309dba534512b6bb
Freen <aboyinsky@outlook.com>
2020-05-28 10:21:34 +0800

    .gitignore

--- apps/game/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'game'
-urlpatterns = [
-    path('', views.game),
-    path('Snake/', views.snake),
-    path('Minesweeper/', views.minesweeper),
-]
