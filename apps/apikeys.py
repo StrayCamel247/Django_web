afa5a3b74b8b8c429fb501e3738a0b33d2c4099d
Freen <aboyinsky@outlook.com>
2020-09-05 19:42:58 +0800

    del all

--- apps/apikeys.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# __author__ : stray_camel
-# __date__: 2020/05/26 12:05:46
-
-#https://user.whoisxmlapi.com/products
-USER_WHOISXMLAPI_KEY = 'at_V7JtWi3Tn43GdUAwCGY7YfOzPk6cm'
