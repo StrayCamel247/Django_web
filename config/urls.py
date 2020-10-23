7a530f31bc4d1bb1e27b4e7c4794e17bf7067c1d
Freen <aboyinsky@outlook.com>
2020-10-23 13:33:39 +0800

    kpi 工厂

--- config/urls.py
+++ config/urls.py
@@ -72,3 +72,5 @@ urlpatterns += [path('', include('apps.apis.views'.format(
     app_name='apis_views')), name='apis_views')]
 urlpatterns += [path('', include('apps.accounts.views'.format(
     app_name='accounts_views')), name='accounts_views')]
+urlpatterns += [path('', include('apps.dashboard.views'.format(
+    app_name='dashboard_views')), name='dashboard_views')]
