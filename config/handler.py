5d262d0903a7dcf062a42859d9896ab77eed858f
Freen <aboyinsky@outlook.com>
2020-12-03 13:24:22 +0800

    [项目启动] 整理启动文件 自测完成

--- config/handler.py
+++ config/handler.py
@@ -12,8 +12,6 @@ def setdefault_django_settings_module():
     项目启动前若不适用默认文件，则需要配置此环境变量
     >>> import os
     >>> os.environ['django_web_flag'] = 'dev'
-
-
     >>> env = os.getenv('django_web_env', 'loc')
     """
     env = os.getenv('django_web_env', 'loc')
--- config/wsgi.py
+++ config/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-
+import sys
+import os
 from django.core.wsgi import get_wsgi_application
 
-from .handler import setdefault_django_settings_module
+
+def setdefault_django_settings_module():
+    """
+    项目启动前若不适用默认文件，则需要配置此环境变量
+    >>> import os
+    >>> os.environ['django_web_flag'] = 'dev'
+    >>> env = os.getenv('django_web_env', 'loc')
+    """
+    env = os.getenv('django_web_env', 'loc')
+    # 设置django默认环境变量 DJANGO_SETTINGS_MODULE
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          'config.{}_settings'.format(env))
+
 
 setdefault_django_settings_module()
 
