732f38bf8068ff4232d2a50a5e629a048e8c9e2e
Freen <aboyinsky@outlook.com>
2021-04-03 00:00:38 +0800

    [env]环境变量

--- config/constants.py
+++ config/constants.py
@@ -5,6 +5,6 @@ import os
 项目启动前若不适用默认文件，则需要配置此环境变量
 >>> import os
 >>> os.environ['django_web_flag'] = 'dev'
->>> env = os.getenv('django_web_env', 'loc')
+>>> env = os.getenv('django_web_flag', 'loc')
 """
-env = os.getenv('django_web_env', 'loc')
+env = os.getenv('django_web_flag', 'loc')
